239321bf4d7ea791f376c928f2f03750f9cbedfc
wesbarron <barron_wesley@columbusstate.edu>
2019-12-04 00:41:28 +0000

    setting things up

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var node = require('node-fetch');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+function getData(){
+    var url = "https://csuserversidewebdevfinal.herokuapp.com/";
+
+    var output = fetch(url);
+    var json = output.json();
+}
+
+
+app.get("/", function(req, res){
+    
+    getData();
+    res.render("index", {
+        json: json
+    });
+});
+
+app.listen(port, function(){
+
+});
